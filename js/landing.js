1413be1263732ced8cd8f69662b8dab135220fc0
prabinarana <ranaprabina554@gmail.com>
2024-07-29 16:58:13 -0400

    some changes

--- js/landing.js
+++ js/landing.js
@@ -10,6 +10,26 @@ var $ = function (id) {
   return document.getElementById(id);
 };
 
+document.addEventListener('DOMContentLoaded', function() {
+  // Retrieve the logged-in user from localStorage
+  const loggedInUser = JSON.parse(localStorage.getItem('loggedInUser'));
+
+  // If a user is logged in, update the welcome message
+  if (loggedInUser) {
+      const usernameElement = document.getElementById('username');
+      usernameElement.textContent = capitalizeFirstLetter(loggedInUser.name);
+  } else {
+      // If no user is logged in, display "Guest"
+      const usernameElement = document.getElementById('username');
+      usernameElement.textContent = 'Guest';
+  }
+});
+
+function capitalizeFirstLetter(string) {
+  if (!string) return ''; // Check if the string is empty or undefined
+  return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
+}
+
 // For slideshow
 let slideIndex = 1;
 showSlides(slideIndex);
@@ -58,11 +78,6 @@ function showSlides(n) {
     var hours = Math.floor((distance % day) / hour);
     var minutes = Math.floor((distance % hour) / minute);
     var seconds = Math.floor((distance % minute) / second);
-
-    console.log(days);
-    console.log(hours);
-    console.log(minutes);
-    console.log(seconds);
 
     document.getElementById("day-timer").innerHTML = days;
     document.getElementById("hour-timer").innerHTML = hours;
